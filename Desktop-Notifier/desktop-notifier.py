d86abacaa9867353a34f0e9170941eb3055fa190
Devansh <201851038@iiitvadodara.ac.in>
2020-08-16 14:05:10 +0530

    Completed the desktop notifier

--- Desktop-Notifier/desktop-notifier.py
+++ Desktop-Notifier/desktop-notifier.py
@@ -1,13 +1,21 @@
 import re
 import requests
+from win10toast import ToastNotifier
 from bs4 import BeautifulSoup as bs
 
-r = requests.get("https://timesofindia.indiatimes.com/")
+try:
+    r = requests.get("https://timesofindia.indiatimes.com/")
+except:
+    r = None
+    print("Check your internet connection")
 
-soup = bs(r.text, "html.parser")
-results = soup.find("div", {"data-vr-zone": "headlines"})
-item = results.find("a", attrs={'href': re.compile("^/")})
-link = item.get("href")
-title = item.get("title")
-print(link)
-print(title)
+if r is not None:
+    soup = bs(r.text, "html.parser")
+    results = soup.find("div", {"data-vr-zone": "headlines"})
+    item = results.find("a", attrs={'href': re.compile("^/")})
+    link = item.get("href")
+    message = item.get("title")
+    title = "Breaking news"
+
+    toaster = ToastNotifier()
+    toaster.show_toast(title, message, icon_path=r'C:\Users\Dell\Downloads\toi.ico', duration=10)
